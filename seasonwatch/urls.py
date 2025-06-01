9814ad6fb538724603b20d23e63b19c640140f9c
wymmij <13812765+wymmij@users.noreply.github.com>
2025-06-01 09:49:15 +0200

    feat(matches): add initial app with placeholder view for routing test

--- seasonwatch/urls.py
+++ seasonwatch/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from matches import views as home_matches
 
 urlpatterns = [
+    path('', home_matches.home, name='home'),
     path("admin/", admin.site.urls),
 ]
