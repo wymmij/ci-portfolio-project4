dcb4b43824e2e6364ae55b942b924947e347d780
wymmij <13812765+wymmij@users.noreply.github.com>
2025-06-01 09:59:07 +0200

    build: ready project for deployment to Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn seasonwatch.wsgi
--- seasonwatch/settings.py
+++ seasonwatch/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-3)yom)iv#(1uso-tboz4p)1j4xs%i#5nbgn2#jk%-&yef2c=@d"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
