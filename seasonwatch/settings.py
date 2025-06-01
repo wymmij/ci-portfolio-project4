7a3daf23d8e0d65cbfbd83039df30c2ed4580743
wymmij <13812765+wymmij@users.noreply.github.com>
2025-06-01 12:29:02 +0200

    build: set local and deployment environment variables

--- seasonwatch/settings.py
+++ seasonwatch/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-3)yom)iv#(1uso-tboz4p)1j4xs%i#5nbgn2#jk%-&yef2c=@d"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
