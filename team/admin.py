21cfa30b4af0de08260b113759b5478ee750f40a
wymmij <13812765+wymmij@users.noreply.github.com>
2025-06-14 20:04:23 +0200

    chore(admin): register Team and Season models for admin access

--- team/admin.py
+++ team/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Team, Season
 
-# Register your models here.
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ('name', 'short_name', 'city', 'country', 'contributor')
+    search_fields = ('name', 'short_name', 'city', 'country')
+
+@admin.register(Season)
+class SeasonAdmin(admin.ModelAdmin):
+    list_display = ('team', 'start_date', 'end_date', 'contributor')
+    list_filter = ('team', 'contributor')
+    search_fields = ('team__name',)
